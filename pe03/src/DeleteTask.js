a78962c5eb1edcdf9b6d452c079f26ede0ce2d4d
JuliaNguyen1220 <108833757+JuliaNguyen1220@users.noreply.github.com>
2023-08-07 06:22:07 +0000

    cs628-pe03-nguyengiang

---
+++ pe03/src/DeleteTask.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DeleteTask({ task, deleteTask }) {
+  const handleDeleteTask = () => {
+    deleteTask(task);
+  };
+
+  return (
+    <button className='delete-task-button' onClick={handleDeleteTask}>
+      Delete</button>
+  );
+}
+
+export default DeleteTask;
